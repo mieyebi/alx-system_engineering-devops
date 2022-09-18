b7eba27d6b4b31dcec8524d104fb1aa3eb8d5bb5
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-18 20:30:38 +0000

    connect to server

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # A manifest that creates a file in /tmp
 
-file {'/tmp/school':
+file {'school':
+  path    => '/tmp/school',
   content => 'I love puppet',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data'
+  group   => 'www-data',
 }
